9e262d9c02ccbd12100cce5cc4d5bf91ee2c8f6b
DGCK81LNN <triluolnn@163.com>
2022-02-22 10:19:44 +0800

    buckle up, we're introducng esbuild

--- lspmt-lib/_build.js
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env node
-
-const fs = require("fs")
-
-var workerCodeBuffer = fs.readFileSync("./worker.js")
-var mainCode = fs.readFileSync("./main.js").toString()
-var dataURL = "data:text/javascript;base64," + workerCodeBuffer.toString("base64")
-mainCode = mainCode.replace('"worker.js"', JSON.stringify(dataURL))
-fs.writeFileSync("./index.js", mainCode)
