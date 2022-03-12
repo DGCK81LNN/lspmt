3dbcd32d89899ee3ff08c4710799e66a566e1e91
DGCK81LNN <triluolnn@163.com>
2022-03-12 13:01:14 +0800

    Create banner.js

---
+++ src/banner.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name         LNNの贴吧多功能图片解密工具
+// @namespace    https://github.com/DGCK81LNN/lspmt
+// @version      [...]
+// @match        https://tieba.baidu.com/p/*
+// @author       DGCK81LNN
+// ==/UserScript==
