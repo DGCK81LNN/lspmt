1c98541857d16e3799db0afc92035fbb6358f013
DGCK81LNN <triluolnn@163.com>
2022-03-12 12:59:17 +0800

    Update inject.mjs

--- src/inject.mjs
+++ src/inject.mjs
@@ -1,13 +1,3 @@
-/*!
-// ==UserScript==
-// @name         LNNの贴吧多功能图片解密工具
-// @namespace    https://github.com/DGCK81LNN/lspmt
-// @version      [update in package.json]
-// @match        https://tieba.baidu.com/p/*
-// @author       DGCK81LNN
-// ==/UserScript==
-*/
-
 import * as LSPMT from "./lib.mjs"
 
 LSPMT.loaded.then(function () {
